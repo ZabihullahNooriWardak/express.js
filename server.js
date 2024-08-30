769515948e73424a68e49bd82346d2dd675c8d4b
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-08-30 06:23:38 +0430

    nothing

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-// just for understanding how req and res works in anonymous function 
-
-class Book{
-  constructor(title,author){
-    this.title=title;
-    this.author=author;
-  }
-  displayinfo(){
-    console.log(`Title: ${this.title} , Author: ${this.author}`);
-  }
-}
-function processBook(bookInstance,callback){
-  callback(bookInstance);
-}
-
-let myBook=new Book('secretBook','ZabihullahNoori');
-myBook.displayinfo();
-processBook(myBook,function(boo){
-  boo.author='antoher author';
-  boo.title='another title';
-})
-myBook.displayinfo();
+const fs = require('fs')
+let zabiData = fs.readFileSync('./zabi.txt','utf-8')
+console.log(zabiData)
